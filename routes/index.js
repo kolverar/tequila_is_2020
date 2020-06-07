b83c584a9f947b6691425f6ffa0e62c90755720d
OniichanUwU <davidcruz99@aragon.unam.mx>
2020-06-07 01:26:22 -0500

    POST y PATCH sin comprobar funcionamiento (tengo yarc :´v)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var Tequila = require('../models/tequila');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tequila' });
+});
+
+router.patch('/tequila/:id', (request, responsive, next)=>{
+	Tequila.findOne({'id': request.params.ID}, (error, datos)=>{
+		if(datos == null){
+			responsive.status(404).json({mensaje:"No hay registro de tal ID"});
+		}else{//Modifica
+			var tequila = Tequila({
+				id : request.body.id
+				nombre : request.body.nombre,
+				empresa : request.body.empresa,
+				tipoAgave : request.body.tipoAgave,
+				porcentajeAlcohol : request.body.porcentajeAlcohol,
+				estadoOrigen : request.body.estadoOrigen,
+				precio : request.body.precio
+			});
+			tequila.patch((error, datos)=>{
+				if(error){
+      				responsive.status(404).json({mensaje:"Error al guardar"});
+    			}else{
+      				responsive.status(201).json(datos);
+    			}
+			});
+		}
+	});
+});
+
+router.post('/tequila', (request, responsive, next)=>{
+	var tequila = Tequila({
+    	id: Number,
+    	nombre: String,
+    	empresa: String,
+    	tipoAgave: String,
+    	porcentajeAlcohol: Number,
+    	estadoOrigen: String,
+    	precio: Number
+	});
+	tequila.save((error, datos)=>{
+		if(error){
+			responsive.status(404).json({mensaje:"Eror inesperado al guardar..."});
+		}else{
+			responsive.status(201).json(datos);
+		}
+	});
 });
 
 module.exports = router;
