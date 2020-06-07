4326f0590be3c6766cc34b664c8b62c79558f225
Jacobo Martinez <jacobomnp@gmail.com>
2020-06-07 15:08:06 -0500

    index restaurado

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'index' });
+  res.render('index', { title: 'Tequila' });
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
