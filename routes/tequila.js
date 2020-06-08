3114641bdd85c6cd5565179b8e230a9ecc8eaff4
Jacobo Martinez <jacobomnp@gmail.com>
2020-06-08 15:26:30 -0500

    metodo delete con confirmación

--- routes/tequila.js
+++ routes/tequila.js
@@ -13,9 +13,16 @@ router.delete('/tequila',(req,res,next)=>{
 router.delete('/tequila/:idTequila' , (req,res,next)=>{
   Tequila.findOneAndDelete({id: req.params.idTequila} , (err, datos)=>{
     if(err){
-        res.status(404).json({mensaje:"No se ha eliminado el producto"});
+        res.status(404).json({mensaje:"No se ha encontrado el producto"});
       }else{
-        res.status(200).json({mensaje:"Se ha eliminado el producto"});
+
+      	var confirmar = confirm("¿Desea eliminar este elemento?");
+
+      	if (confirmar==true) {
+        	res.status(200).json({mensaje:"Se ha eliminado el producto"});
+    	}else{
+    		res.status(405).json({mensaje:"Acción cancelada"});
+    	}
       }
   });
 });
