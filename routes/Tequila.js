77b847a6fca6a6e2ff0e73ec59a6a6f52f866772
OniichanUwU <davidcruz99@aragon.unam.mx>
2020-06-09 23:33:54 -0500

    añadido delete

--- routes/Tequila.js
+++ routes/Tequila.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Tequila = require('../models/tequila');
-
+/*
 router.get('/tequila/:tequilaID',(req,res,next)=>{
 	Tequila.findOne({'id' : req.params.tequilaID},(err,datos)=>{
     	if( datos == null){
@@ -10,6 +10,14 @@ router.get('/tequila/:tequilaID',(req,res,next)=>{
       	}else{
         	res.status(200).json(datos);
       	}
+    });
+});
+*/
+router.get('/tequila/', (req, res, next) => { //kevin
+    Tequila.find({}, (err, data) => {
+        if(err) res.status(500).json({error: "Error!"})
+        if(data) console.log(data) 
+          res.status(200).json(data)
     });
 });
 
@@ -48,6 +56,25 @@ router.patch('/tequila/:tequilaID', (request, responsive, next)=>{
     		responsive.status(201).json(datos);
     	}
   	});
+});
+
+confirmar = new Boolean;
+router.delete('/tequila',(req,res,next)=>{
+  res.status(405).json({mensaje:"No permitido"});
+});
+router.delete('/tequila/:idTequila' , (req,res,next)=>{
+  Tequila.findOneAndDelete({id: req.params.idTequila} , (err, datos)=>{
+    if(err){
+        res.status(404).json({mensaje:"No se ha encontrado el producto"});
+      }else{
+        var confirmar = confirm("¿Desea eliminar este elemento?");
+        if (confirmar==true) {
+          res.status(200).json({mensaje:"Se ha eliminado el producto"});
+      }else{
+        res.status(405).json({mensaje:"Acción cancelada"});
+      }
+    }
+  });
 });
 
 module.exports = router;
