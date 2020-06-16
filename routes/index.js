92a7c2497e597cd34299dfdac9e7dd948c35dfa1
lesterxd <lesterelcrack7@gmail.com>
2020-06-15 23:15:51 -0500

    solucionando error head

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.render('index', { title: 'index' });
 });
-
 module.exports = router;
-=======
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
->>>>>>> ramajac
