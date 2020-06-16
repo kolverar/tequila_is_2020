bdd8397c601e32fd1060e996c86b50d8fb8bb8ee
lesterxd <lesterelcrack7@gmail.com>
2020-06-15 19:00:54 -0500

    mejora de interfaz en buscar, y en error 404

--- routes/tequila.js
+++ routes/tequila.js
@@ -1,94 +1,53 @@
-router.use((req, res, next) => {
+const express = require('express');
+const router = express.Router();
 
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
 
-router.get('/tequila/',(req,res,netxt)=>{
-    Tequila.find({},(err,datos)=>{
 
-        if(err) res.status(500).json({error:"No existe!"});
-        if(datos)
+
+let Tequila = require('../models/tequila')
+
+router.get('/tequila/', (req, res, next) => {
+  res.render('tequilaGet',{nombre:'kaskksa'})
+});
+//metodo get para buscar por nombre desde url
+router.get('/tequila/:nombreT',(req, res, next) =>{
+  Tequila.findOne(
+    {'nombre':req.params.nombreT},
+    (err,datos)=>{
+      if(datos==null){
+        res.status(404).render('error404',{error:'404'})
+      }else{
+        res.status(200).json(datos);
+      }
+    }
+  );
+  });
+  //metodo post para obtener datos del la opcion buscar por nombre desde formulario
+  router.post('/tequila/', (req, res, next) => {
+    if(req.body.botonNom=='1'){
+      Tequila.findOne(
+        {'nombre':req.body.pstNom},
+        (err,datos)=>{
+          if(datos==null){
+            res.status(404).render('error404',{error:'404'})
+          }else{
             res.status(200).json(datos);
-
-    });
-});
-
-router.get('/tequila/:idTequila',(req,res,next)=>{
-    Tequila.findOne({'id' : req.params.idTequila},(err,datos)=>{
-
-        if( datos == null){
-            res.status(404).json({mensaje:"No existe!"});
-        }else{
+          }
+        }
+      );
+    }else {
+      //metodo post para obtener datos del la opcion buscar por nombre desde formulario
+      Tequila.findOne(
+        {'precio':req.body.pstPrec},
+        (err,datos)=>{
+          if(datos==null){
+            res.status(404).render('error404',{error:'404'})
+          }else{
             res.status(200).json(datos);
+          }
         }
-    });
-});
-
-
-router.post('/tequila',(req,res,next)=>{
-
-    var tequila = Tequila(
-        {
-            id:req.body.id,
-            nombre:req.body.nombre,
-            empresa:req.body.empresa,
-            tipoAgave:req.body.tipoAgave,
-            porcentajeAlcohol:req.body.porcentajeAlcohol,
-            estadoOrigen:req.body.estadoOrigen,
-            precio:req.body.precio
-        }
-    );
-    tequila.save((err,datos)=>{
-        if(err){
-            res.status(404).json({
-                mensaje:"Error al guardar"
-            });
-        }else{
-            res.status(201).json(datos);
-        }
-    });
-});
-
-
-router.delete('/tequila',(req,res,next)=>{
-
-    res.status(405).json({mensaje:"No permitido"});
-});
-
-router.delete('/tequila/:idTequila' , (req,res,next)=>{
-
-    Tequila.findOneAndDelete({id: req.params.idTequila} , (err, datos)=>{
-        if(err){
-            res.status(404).json({mensaje:"No se ha encontrado el producto"});
-        }else
-        if(datos){
-
-            res.status(200).json({mensaje: "Ok"})
-        }
-    });
-});
-
-
-router.patch('/tequila/:tequilaID', (request, responsive, next)=>{
-
-    Tequila.findOneAndUpdate({id : request.params.tequilaID},{
-        id : request.body.id,
-        nombre : request.body.nombre,
-        empresa : request.body.empresa,
-        tipoAgave : request.body.tipoAgave,
-        porcentajeAlcohol : request.body.porcentajeAlcohol,
-        estadoOrigen : request.body.estadoOrigen,
-        precio : request.body.precio
-    },function(error,datos){
-        if (error) {
-            responsive.status(404).json({mensaje:"Error al guardar"});
-        }else{
-            responsive.status(201).json(datos);
-        }
-    });
-});
+      );
+    }
+  });
 
 module.exports = router;
