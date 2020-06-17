b1699114a80ba6eaf8d85b1ab25e5bcd5c166ac5
kolverar <kevincristian@outlook.com>
2020-06-17 17:59:45 -0500

    Heroku

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/tequila/api", tequilaRouter)
+app.use("/tequila/api/", tequilaRouter)
 
 
 // catch 404 and forward to error handler
--- routes/tequila.js
+++ routes/tequila.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Tequila = require('../models/tequila');
 
+
 router.use((req, res, next) => {
 
     res.header("Access-Control-Allow-Origin", "*")
@@ -11,6 +12,32 @@ router.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
+
+
+//get para obtener todos los registros
+router.get('/tequila/', (req, res, next) => {
+    Tequila.find({}, (err, data) => {
+        if(err)
+            res.status(500).render('error404',{error:'404'})
+        if(data)
+            console.log(data)
+        res.status(200).json(data)
+    });
+});
+
+//metodo get para buscar por nombre desde url
+router.get('/tequila/:nombreT',(req, res, next) =>{
+    Tequila.findOne(
+        {'nombre':req.params.nombreT},
+        (err,datos)=>{
+            if(datos==null){
+                res.status(404).render('error404',{error:'404'})
+            }else{
+                res.status(200).json(datos);
+            }
+        }
+    );
+});
 
 router.delete('/tequila',(req,res,next)=>{
 
