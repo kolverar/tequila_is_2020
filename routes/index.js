0e1af56b5b39ccd6602dcb9fe0f4e2e829744d3b
kolverar <kevincristian@outlook.com>
2020-06-16 21:34:10 -0500

    Router Index in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'index' });
+  res.render("index")
 });
 
 //get para obtener opciones de buscar
